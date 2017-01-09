f8593a651fb24558c1e828a5f7185068532ed82f
Dirk Hörner <dirker@gmail.com>
2017-01-09 22:12:50 +0100

    Remove boomplay code, going with mpg123 as backend
    Eventually, I discovered that the mpg123 remote feature has everything I
    needed, and more :-)

--- boomplay/playlist.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PLAYLIST_H
-#define __PLAYLIST_H
-
-int playlist_add(const char *fname);
-int playlist_seek(unsigned int pos);
-const char *playlist_next(void);
-void playlist_wipe(void);
-
-#endif
